82b5cab5e100bd826a0083b873c3ef3f957ca1fa
yoonback <108972911+yoonback@users.noreply.github.com>
2022-07-09 17:09:56 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.facebook.com/profile,php?id=100044216387048" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["black","white"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "윤건우")
+write("description"남자,중학생")
+write("button", "프로필")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "나이": "15",
+  "학교": "공산중학교",
+  "신분": "학생",
+  "가족관계": "엄마,아빠,나"
 }
 information(informations)
